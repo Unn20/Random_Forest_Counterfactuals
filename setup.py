704d9a011c909677795de6302ca0c0d99a46f9bc
Maciej Leszczyk <maciej.leszczyk@palturai.com>
2022-08-16 16:17:23 +0200

    Fixed bug with Pareto selection

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 setup(
     name='rf_counterfactuals',
     packages=find_packages(),
-    version='0.1.5',
+    version='0.1.6',
     description='Random Forest counterfactual explanation',
     keywords='counterfactual, explainable, xai, random forest',
     long_description=long_description,
